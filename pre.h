110227b7a0680d30f34024d039c81518d6cf6873
Nguyễn Bảo Long <nguyenbaolong@MacBook-Pro-cua-Nguyen.local>
2020-05-01 13:37:47 +0700

    pre compile

---
+++ pre.h
@@ -0,0 +1,16 @@
+#ifndef PRE_H_
+#define PRE_H_
+
+#include "stdio.h"
+#include "stdlib.h"
+
+#include "iostream"
+using namespace std;
+
+#include "string"
+
+// viết code thì include file này vào,, 
+// add thư viện vào đây nhá :)) 
+// chỉ add những thư viện do c/c++ hỗ trợ vào đây thoi,, 
+
+#endif
